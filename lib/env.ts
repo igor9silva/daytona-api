fa94c676289205150f1489075f3f33fc714bc585
Igor Silva <igor@igorsilva.pro>
2025-07-12 12:36:08 +0100

    Initial Commit

---
+++ lib/env.ts
@@ -0,0 +1,27 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
+
+export const env = createEnv({
+	//
+	runtimeEnv: process.env,
+
+	server: {
+		//
+		PORT: z.coerce.number().default(4000).describe('Server port.'),
+		DAYTONA_API_KEY: z.string().min(1).describe('Daytona API key.'),
+	},
+
+	/**
+	 * By default, this library will feed the environment variables directly to
+	 * the Zod validator.
+	 *
+	 * This means that if you have an empty string for a value that is supposed
+	 * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
+	 * it as a type mismatch violation. Additionally, if you have an empty string
+	 * for a value that is supposed to be a string with a default value (e.g.
+	 * `DOMAIN=` in an ".env" file), the default value will never be applied.
+	 *
+	 * This is true in order to solve these issues.
+	 */
+	emptyStringAsUndefined: true,
+});
